08feae370995d8c7ccb8eea0e82b53b2c6317e82
laluisari-at <=>
2023-12-26 17:49:30 +0800

    phase 2 done

--- app/Http/Controllers/NewProductController.php
+++ app/Http/Controllers/NewProductController.php
@@ -105,8 +105,65 @@ class NewProductController extends Controller
      */
     public function update(Request $request, New_product $new_product)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'code_document' => 'required',
+            'old_barcode_product' => 'required',
+            'new_barcode_product' => 'required',
+            'new_name_product' => 'required',
+            'new_quantity_product' => 'required|integer',
+            'new_price_product' => 'required|numeric',
+            'new_date_in_product' => 'required|date',
+            'new_status_product' => 'required|in:display,expired,promo,bundle,palet',
+            'condition' => 'required|in:lolos,damaged,abnormal',
+            'new_category_product' => 'nullable|exists:categories,name_category',
+            'new_tag_product' => 'nullable|exists:color_tags,name_color'
+        ]);
+    
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+    
+        $status = $request->input('condition');
+        $description = $request->input('deskripsi', '');
+    
+        $qualityData = [
+            'lolos' => $status === 'lolos' ? 'lolos' : null,
+            'damaged' => $status === 'damaged' ? $description : null,
+            'abnormal' => $status === 'abnormal' ? $description : null,
+        ];
+    
+        $inputData = $request->only([
+            'code_document',
+            'old_barcode_product',
+            'new_barcode_product',
+            'new_name_product',
+            'new_quantity_product',
+            'new_price_product',
+            'new_date_in_product',
+            'new_status_product',
+            'new_category_product',
+            'new_tag_product'
+        ]);
+    
+        if ($status !== 'lolos') {
+            // Set nilai-nilai default jika status bukan 'lolos'
+            $inputData['new_status_product'] = 'display';
+            $inputData['new_quantity_product'] = 0;
+            $inputData['new_price_product'] = 0;
+            $inputData['new_category_product'] = null;
+            $inputData['new_tag_product'] = null;
+            $inputData['new_name_product'] = null;
+            $inputData['new_barcode_product'] = null;
+        }
+    
+        $inputData['new_quality'] = json_encode($qualityData);
+
+    
+        $new_product->update($inputData);
+    
+        return new ResponseResource(true, "New Produk Berhasil di Update", $new_product);
     }
+    
 
     /**
      * Remove the specified resource from storage.
