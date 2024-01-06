67f37b072cfa1819a542072d2e5390f82acb3577
laluisari-at <=>
2024-01-06 18:48:16 +0800

    update document for delete

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -19,51 +19,43 @@ class DocumentController extends Controller
         return new ResponseResource(true, "List Documents", $documents);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+ 
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+  
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
+  
     public function show(Document $document)
     {
         return new ResponseResource(true, "detail document", $document);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Document $document)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+ 
     public function update(Request $request, Document $document)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+   
     public function destroy(Document $document)
     {
-        //
+        try{
+            $document->delete();
+            return new ResponseResource(true, "data berhasil dihapus", $document);
+        }catch (\Exception $e){
+            return new ResponseResource(false, "terjadi kesalahan saat menghapus data", null);
+        }
     }
 }
