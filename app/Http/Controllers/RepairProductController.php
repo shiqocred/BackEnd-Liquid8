5efddbfa6e713c86e515b5868a60c13e7e04b1eb
Hayyi Shinshan Mahesa <hayyi123sm@gmail.com>
2024-03-28 12:03:27 +0700

    update again RepairProductController > method updateRepair

--- app/Http/Controllers/RepairProductController.php
+++ app/Http/Controllers/RepairProductController.php
@@ -172,9 +172,17 @@ class RepairProductController extends Controller
 
             New_product::create([
                 "code_document" => $product->code_document,
+                "old_barcode_product" => $product->old_barcode_product,
                 "new_barcode_product" => $product->new_barcode_product,
                 "new_name_product" => $product->new_name_product,
-
+                "new_quantity_product" => $product->new_quantity_product,
+                "new_price_product" => $product->new_price_product,
+                "old_price_product" => $product->old_price_product,
+                "new_date_in_product" => $product->new_date_in_product,
+                "new_status_product" => $product->new_status_product,
+                "new_quality" => $product->new_quality,
+                "new_category_product" => $product->new_category_product,
+                "new_tag_product" => $product->new_tag_product,
             ]);
 
             // Hancurkan objek produk
