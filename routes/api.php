9642a688cc85c3c531388d12b1b1df29f242aa3f
laluisari-at <=>
2024-04-20 15:46:13 +0700

    membersihkan commentar di function addproductbyadmin  -v2

--- routes/api.php
+++ routes/api.php
@@ -296,3 +296,4 @@ Route::post('login', [AuthController::class, 'login']);
 Route::delete('cleargenerate', [GenerateController::class, 'deleteAll']);
 
 Route::delete('deleteAll', [GenerateController::class, 'deleteAllData']);
+Route::delete2('deleteAll', [GenerateController::class, 'deleteAllData']);
