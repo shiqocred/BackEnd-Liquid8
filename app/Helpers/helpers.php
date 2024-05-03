2f7a3efe82ece8798bbd9716837a714eff669f4c
Hayyi Shinshan Mahesa <hayyi123sm@gmail.com>
2024-05-03 15:18:06 +0700

    fix: bug codeDocumentSale by userId

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -2,7 +2,9 @@
 
 use App\Models\Migrate;
 use App\Models\MigrateDocument;
+use App\Models\Sale;
 use App\Models\SaleDocument;
+use Illuminate\Support\Facades\DB;
 
 function codeDocumentMigrate()
 {
@@ -19,13 +21,32 @@ function codeDocumentMigrate()
     return $code;
 }
 
-function codeDocumentSale()
+function codeDocumentSale($userId)
 {
-    $codeDocumentMigrate = SaleDocument::selectRaw('MAX(MID(code_document_sale, 7, 5)) as code_document_sale')
+    $codeDocumentSale = SaleDocument::selectRaw('MAX(MID(code_document_sale, 7, 5)) as code_document_sale')
         ->first();
 
-    if ($codeDocumentMigrate->count() > 0) {
-        $n = ((int)$codeDocumentMigrate['code_document_sale']) + 1;
+    $sales = Sale::where('status_sale', 'proses')
+        ->select('user_id', DB::raw('MAX(MID(code_document_sale, 7, 5)) as code_document_sale'))
+        ->groupBy('user_id')
+        ->get();
+
+    if ($codeDocumentSale->count() > 0) {
+        if ($sales->count() > 0) {
+            foreach ($sales as $sale) {
+                if ($userId == $sale->user_id) {
+                    $n = ((int)$sale['code_document_sale']);
+                    break;
+                } else {
+                    $n = ((int)$codeDocumentSale['code_document_sale']) + 1;
+                    if ($sale['code_document_sale'] == $codeDocumentSale['code_document_sale'] + 1) {
+                        $n += 1;
+                    }
+                }
+            }
+        } else {
+            $n = ((int)$codeDocumentSale['code_document_sale']) + 1;
+        }
         $no = sprintf("%'.05d", $n);
     } else {
         $no = "00001";
