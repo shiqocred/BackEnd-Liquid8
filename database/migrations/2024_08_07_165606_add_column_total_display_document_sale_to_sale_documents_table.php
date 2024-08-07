cf81071618912bfc297aaac7d4007ee98ba37dd0
Hayyi Shinshan Mahesa <hayyi123sm@gmail.com>
2024-08-08 00:13:07 +0800

    feat: add attribute old product and display product to saledocument & feature dashboard

---
+++ database/migrations/2024_08_07_165606_add_column_total_display_document_sale_to_sale_documents_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('sale_documents', function (Blueprint $table) {
+            $table->decimal('total_display_document_sale', 15, 2)->after('total_price_document_sale');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('sale_documents', function (Blueprint $table) {
+            $table->dropColumn('total_display_document_sale');
+        });
+    }
+};
