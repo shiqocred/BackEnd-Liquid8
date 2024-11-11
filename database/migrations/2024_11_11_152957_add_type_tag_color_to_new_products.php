6c9c24302fa42bb2d370a6238b43578f90bd3e47
laluisari <laluisari@gmail.com>
2024-11-12 05:52:41 +0700

    menambahkan kolom di migration

--- database/migrations/2024_11_11_152957_add_type_tag_color_to_new_products.php
+++ database/migrations/2024_11_11_152957_add_type_tag_color_to_new_products.php
@@ -12,17 +12,107 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('new_products', function (Blueprint $table) {
-            //
+            $table->enum('type_color', ['type_color1', 'type_color2'])->nullable();
+        });
+        Schema::table('product__filters', function (Blueprint $table) {
+            $table->enum('type_color', ['type_color1', 'type_color2'])->nullable();
+        });
+        Schema::table('product__bundles', function (Blueprint $table) {
+            $table->enum('type_color', ['type_color1', 'type_color2'])->nullable();
+        });
+        Schema::table('palet_filters', function (Blueprint $table) {
+            $table->enum('type_color', ['type_color1', 'type_color2'])->nullable();
+        });
+        Schema::table('palet_products', function (Blueprint $table) {
+            $table->enum('type_color', ['type_color1', 'type_color2'])->nullable();
+        });
+        Schema::table('repair_products', function (Blueprint $table) {
+            $table->enum('type_color', ['type_color1', 'type_color2'])->nullable();
+        });
+        Schema::table('repair_filters', function (Blueprint $table) {
+            $table->enum('type_color', ['type_color1', 'type_color2'])->nullable();
+        });
+        Schema::table('product_approves', function (Blueprint $table) {
+            $table->enum('type_color', ['type_color1', 'type_color2'])->nullable();
+        });
+        Schema::table('product_qcds', function (Blueprint $table) {
+            $table->enum('type_color', ['type_color1', 'type_color2'])->nullable();
+        });
+        Schema::table('filter_qcds', function (Blueprint $table) {
+            $table->enum('type_color', ['type_color1', 'type_color2'])->nullable();
+        });
+        Schema::table('sales', function (Blueprint $table) {
+            $table->enum('type_color', ['type_color1', 'type_color2'])->nullable();
+        });
+        Schema::table('filter_product_inputs', function (Blueprint $table) {
+            $table->enum('type_color', ['type_color1', 'type_color2'])->nullable();
+        });
+        Schema::table('filter_stagings', function (Blueprint $table) {
+            $table->enum('type_color', ['type_color1', 'type_color2'])->nullable();
+        });
+        Schema::table('product_inputs', function (Blueprint $table) {
+            $table->enum('type_color', ['type_color1', 'type_color2'])->nullable();
+        });
+        Schema::table('staging_approves', function (Blueprint $table) {
+            $table->enum('type_color', ['type_color1', 'type_color2'])->nullable();
+        });
+        Schema::table('staging_products', function (Blueprint $table) {
+            $table->enum('type_color', ['type_color1', 'type_color2'])->nullable();
         });
     }
-
+    
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
         Schema::table('new_products', function (Blueprint $table) {
-            //
+            $table->dropColumn('type_color');
+        });
+        Schema::table('product__filters', function (Blueprint $table) {
+            $table->dropColumn('type_color');
+        });
+        Schema::table('product__bundles', function (Blueprint $table) {
+            $table->dropColumn('type_color');
+        });
+        Schema::table('palet_filters', function (Blueprint $table) {
+            $table->dropColumn('type_color');
+        });
+        Schema::table('palet_products', function (Blueprint $table) {
+            $table->dropColumn('type_color');
+        });
+        Schema::table('repair_products', function (Blueprint $table) {
+            $table->dropColumn('type_color');
+        });
+        Schema::table('repair_filters', function (Blueprint $table) {
+            $table->dropColumn('type_color');
+        });
+        Schema::table('product_approves', function (Blueprint $table) {
+            $table->dropColumn('type_color');
+        });
+        Schema::table('product_qcds', function (Blueprint $table) {
+            $table->dropColumn('type_color');
+        });
+        Schema::table('filter_qcds', function (Blueprint $table) {
+            $table->dropColumn('type_color');
+        });
+        Schema::table('sales', function (Blueprint $table) {
+            $table->dropColumn('type_color');
+        });
+        Schema::table('filter_product_inputs', function (Blueprint $table) {
+            $table->dropColumn('type_color');
+        });
+        Schema::table('filter_stagings', function (Blueprint $table) {
+            $table->dropColumn('type_color');
+        });
+        Schema::table('product_inputs', function (Blueprint $table) {
+            $table->dropColumn('type_color');
+        });
+        Schema::table('staging_approves', function (Blueprint $table) {
+            $table->dropColumn('type_color');
+        });
+        Schema::table('staging_products', function (Blueprint $table) {
+            $table->dropColumn('type_color');
         });
     }
 };
