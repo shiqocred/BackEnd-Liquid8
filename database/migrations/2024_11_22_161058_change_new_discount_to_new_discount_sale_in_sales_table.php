4c51618f1eb07e4f7a1c6c5411db63610bae3689
Hayyi Shinshan Mahesa <hayyi123sm@gmail.com>
2024-11-22 22:42:28 +0800

    feat: add feature discount sale for new logic cashier

---
+++ database/migrations/2024_11_22_161058_change_new_discount_to_new_discount_sale_in_sales_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('sales', function (Blueprint $table) {
+            $table->renameColumn('new_discount', 'new_discount_sale');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('sales', function (Blueprint $table) {
+            $table->renameColumn('new_discount_sale', 'new_discount');
+        });
+    }
+};
